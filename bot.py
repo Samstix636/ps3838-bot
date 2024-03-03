f511306b9f24748a4ce845baf428f8b435fccb1e
samstix636 <ekokotasamuel70@gmail.com>
2024-03-03 22:18:19 +0100

    add func to parse bet info

--- bot.py
+++ bot.py
@@ -5,8 +5,13 @@ import requests
 import time
 from datetime import datetime
 
+
+
 def parse_bb_tips():
-    for i in range(50):
+    f = open('results.json', 'a')
+    valid_bets = []
+    while True:
+        time.sleep(10)
         url = "https://rest-api-pr.betburger.com/api/v1/valuebets/bot_pro_search"
         headers = {
             'accept': "application/json",
@@ -15,41 +20,41 @@ def parse_bb_tips():
         
 
         response = requests.request("POST", url=url, headers=headers, params={
-            'access_token': "token",
+            'access_token': "36971d55b71eea2308d5827cbe1e75e1", #bcdb92ef761f026b0f2b4737dddcabfe
             'per_page': 50,
-            'search_filter': [370233]
+            'search_filter': [396072] #765428 #396072
         })
-        # print(response.url)
-        # pprint(response, sort_dicts=False)
         
         response = response.json()
-        print(response)
+        
+        # pprint(response)
         bets = response['bets']
-        valid_bets = []
+        
         for b in bets:
             try:
-                if b['is_value_bet']:
-                    valid_bets.append(b)
+                if b['is_value_bet'] is True and b['id'] not in valid_bets:
+                    valid_bets.append(b['id'])
+                    json_obj = json.dumps(b, indent=4)
+                    f.write(json_obj)
             except:
                 pass
         bb_events = []
-        for bet in valid_bets:
-            data = {}
-            data['away'] = bet['away']
-            data['home'] = bet['home']
-            data['league'] = bet['league_name']
-            data['price'] = bet['koef']
-            # data['price'] = bet['koef']
-            # data['bookmaker'] = bookmakers[bet['bookmaker_id']]
-            data['bet_type'] = bet['market_and_bet_type']
-            data['bet_type_value'] = bet['market_and_bet_type_param']
-            data['sport_id'] = bet['sport_id']
-            try:
-                data['current_score'] = bet['current_score'].split(' ')[0]
-            except:
-                pass
-            data['period_id'] = bet['period_id']
-            data['start_time'] = datetime.fromtimestamp(bet['started_at']-3600).strftime('%Y-%m-%dT%H:%M:%SZ')
-            bb_events.append(data)
+        # for bet in valid_bets:
+        #     data = {}
+        #     data['away'] = bet['away']
+        #     data['home'] = bet['home']
+        #     data['league'] = bet['league_name']
+        #     data['price'] = bet['koef']
+        #     data['bet_type'] = bet['market_and_bet_type']
+        #     data['bet_type_value'] = bet['market_and_bet_type_param']
+        #     data['sport_id'] = bet['sport_id']
+        #     try:
+        #         data['current_score'] = bet['current_score'].split(' ')[0]
+        #     except:
+        #         pass
+        #     data['period_id'] = bet['period_id']
+        #     data['start_time'] = datetime.fromtimestamp(bet['started_at']-3600).strftime('%Y-%m-%dT%H:%M:%SZ')
+        #     bb_events.append(data)
+    f.close()
 
-
+parse_bb_tips()
--- utils.py
+++ utils.py
@@ -1,16 +1,10 @@
 import base64
 import requests
 from enum import Enum
-
-"""
-Script to connect to ps3838 API
-URL to check API User Guide:
-https://www.tender88.com/static/index.php/es-es/help/api-user-guide-es-es
-In order to access PS3838 API you must have a funded account.
-"""
+from pprint import pprint
 
 # API ENDPOINT
-API_ENDPOINT = 'http://api.ps3838.com'
+API_ENDPOINT = 'https://api.ps3838.com'
 
 
 # Available Request Methods
@@ -20,8 +14,8 @@ class HttpMethod(Enum):
 
 
 # Constants to fill by each user
-PS3838_USERNAME = "FILL_USERNAME_HERE"
-PS3838_PASSWORD = "FILL_PASSWORD_HERE"
+PS3838_USERNAME = "BIA0003Q91"
+PS3838_PASSWORD = "Iowahawkeyes1@"
 
 
 def get_headers(request_method: HttpMethod) -> dict:
@@ -31,7 +25,9 @@ def get_headers(request_method: HttpMethod) -> dict:
         headers.update({'Content-Type': 'application/json'})
 
     headers.update({'Authorization': 'Basic {}'.format(
-        base64.b64encode((bytes("{}:{}".format(PS3838_USERNAME, PS3838_PASSWORD), 'utf-8'))).decode())
+        base64.b64encode((bytes("{}:{}".format(PS3838_USERNAME, PS3838_PASSWORD), 'utf-8'))).decode()),
+                    'Accept': 'application/json',
+                    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
     })
 
     return headers
@@ -42,7 +38,15 @@ def get_operation_endpoint(operation: str) -> str:
 
 
 def get_sports():
-    operation = '/v1/sports'
+    operation = '/v3/sports'
+    req = requests.get(
+        get_operation_endpoint(operation),
+        headers=get_headers(HttpMethod.GET)
+    )
+    return req.json()
+
+def get_balance():
+    operation = '/v2/client/balance'
     req = requests.get(
         get_operation_endpoint(operation),
         headers=get_headers(HttpMethod.GET)
@@ -50,5 +54,76 @@ def get_sports():
     return req.json()
 
 
+def get_period():
+    operation = '/v1/periods'
+    req = requests.get(
+        get_operation_endpoint(operation),
+        headers=get_headers(HttpMethod.GET),
+        params= {'sportId': 29}
+    )
+    return req.json()
+
+
 # Test retrieve sports endpoint
-print(get_sports())
+# pprint(get_sports())
+pprint(get_period())
+
+
+def parse_bet_type(info):
+    if info == 'S':
+        bet_type = 'SPREAD'
+    elif info == 'T':
+        bet_type = 'TOTAL_POINTS'
+    elif info == 'M':
+        bet_type = 'MONEYLINE'
+    elif info == 'I':
+        bet_type = 'TEAM_TOTAL_POINTS'
+    else:
+        bet_type = None
+    return bet_type
+
+def parse_team(id):
+    if id == '0':
+        team = 'TEAM1'
+    elif id == '1':
+        team = 'TEAM2'
+    elif id == '2':
+        team = 'DRAW'
+    
+    return team
+
+def parse_side(id):
+    if id == '0':
+        side = 'OVER'
+    elif id == '1':
+        side = 'UNDER'
+
+def parse_bet_info(bet_info):
+    # 1586333564/2426942860||I|0/0/3/0/1.763?line=0.5
+    bet_link = bet_info['direct_link']
+    line = bet_info['market_and_bet_type_param']
+    
+    bet_data = {}
+    bet_link = bet_link.replace('/', '|')
+    data = bet_link.split('|')
+    bet_data['event_id'] = data[0]
+    bet_data['line_id'] = data[1]
+    bet_type = parse_bet_type(data[3])
+    bet_data['bet_type'] = bet_type
+    bet_data['period_id'] = data[4]
+    if bet_type == 'MONEYLINE':
+        bet_data['handicap'] = None
+        bet_data['side'] = None
+        bet_data['team'] = parse_team(data[5])
+    elif bet_type == 'TOTAL POINTS':
+        bet_data['handicap'] = line
+        bet_data['side'] = None
+        bet_data['team'] = parse_team(data[5])
+        
+    
+    
+    
+
+
+    
+        
